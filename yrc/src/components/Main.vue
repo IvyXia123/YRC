ed94c7b3d1bd38c2724613be2744f2787507ac0d
IvyXia123 <ivyHsia@126.com>
2017-10-28 22:47:01 +0800

    add login page

---
+++ yrc/src/components/Main.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="main">
+    <header-com></header-com>
+  </div>
+</template>
+
+<script>
+  import HeaderCom from './Header/Header'
+export default {
+  name: 'main',
+  data () {
+    return {
+
+    }
+  },
+  components:{
+    HeaderCom
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- yrc/src/router/index.js
+++ yrc/src/router/index.js
@@ -1,15 +1,23 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Main from '@/components/Main'
+import Login from '@/components/Login/Login.vue'
 
 Vue.use(Router)
 
 export default new Router({
+  mode:'history',
+  base:__dirname,
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: HelloWorld
+      name: 'Main',
+      component: Main
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login,
     }
   ]
 })
