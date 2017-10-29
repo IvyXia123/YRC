b9a1947d9e82ef29dea6be05e585c71756f9360f
IvyXia123 <ivyHsia@126.com>
2017-10-29 15:16:25 +0800

    uuu

--- yrc/src/main.js
+++ yrc/src/main.js
@@ -6,7 +6,7 @@ import router from './router'
 import '!style-loader!css-loader!less-loader!./less/init.less'
 import './js/rem.js'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
