3272ae1c3fc8bb6097bc9d78ed5852b97b78e72d
wangkai <wangkai@adinnet.cn>
2017-10-29 14:59:45 +0800

    jjj

--- yrc/src/components/Main.vue
+++ yrc/src/components/Main.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="main">
     <header-com></header-com>
+    <v-banner></v-banner>
     <v-footer></v-footer>
   </div>
 </template>
 
 <script>
-  import HeaderCom from './Header/Header';
-  import footer from './footer/footer';
+  import HeaderCom from './Header/Header';  // 头部
+  import footer from './footer/footer';     // 底部
+  import banner from './home/banner';       // 轮播图
 export default {
   name: 'main',
   data () {
@@ -17,12 +19,12 @@ export default {
   },
   components:{
     HeaderCom,
-    'v-footer': footer
+    'v-footer': footer,
+    'v-banner': banner
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
 </style>
